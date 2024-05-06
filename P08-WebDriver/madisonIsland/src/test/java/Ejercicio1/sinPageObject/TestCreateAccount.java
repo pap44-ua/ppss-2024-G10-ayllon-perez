325a6999ffd95c8e8d81e7a65aad96932983a175
Paula Ayllon Perez <pap44@gcloud.ua.es>
2024-05-06 20:18:21 +0200

    finalizada pract 7 y empezada la P8

---
+++ P08-WebDriver/madisonIsland/src/test/java/Ejercicio1/sinPageObject/TestCreateAccount.java
@@ -0,0 +1,11 @@
+package Ejercicio1.sinPageObject;
+
+public class TestCreateAccount {
+
+    @Test
+    public void S1_scenario_createAccount_should_create_new_account_in_the_demo_store_when_this_account_does_not_exist()
+    {
+
+    }
+
+}
